9fe011142429cfd79cb809f3e7fe1b4daa22b2b9
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-08-03 13:16:35 +0200

    add angular material

--- apps/purchase-manager/src/app/app.component.ts
+++ apps/purchase-manager/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardFooter,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    RouterModule,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardContent,
+    MatCardFooter,
+    MatCardActions,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
