84e9b58ff17c40013d8e381deb8b63eab2498653
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-08-03 12:46:00 +0200

    init nx monorepo

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PurchaseManagerComponent } from './components/purchase-manager/purchase-manager.component';
-import { TodoComponent } from './components/todo/todo.component';
-
-export const routes: Routes = [
-  { path: 'purchase-manager', component: PurchaseManagerComponent },
-  { path: 'todo', component: TodoComponent },
-];
--- src/app/models/app-option.ts
+++
@@ -1,4 +0,0 @@
-export interface AppOption {
-  name: string;
-  url: string;
-}
