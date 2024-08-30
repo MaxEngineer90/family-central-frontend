cf1a62e8b97010082d80293ff04d5e1faca93bbb
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-08-30 19:49:11 +0200

    delete lib ui

--- apps/purchase-manager/src/app/app.component.ts
+++ apps/purchase-manager/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { AppChooserComponent } from '@family-central-frontend/app-chooser';
+import { MatToolbar } from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [
-    AppChooserComponent
-  ],
+  imports: [AppChooserComponent, MatToolbar, NgOptimizedImage],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'purchase-manager';
+  protected readonly logoPath = '/images/logo.png';
 }
