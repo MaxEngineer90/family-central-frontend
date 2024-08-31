8e431064380c61fafd1c07e97a46c77721cb2605
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-08-31 11:31:12 +0200

    add cypress tests

---
+++ apps/family-central-web-e2e/src/e2e/app-chooser.cy.ts
@@ -0,0 +1,14 @@
+describe('app-chooser', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should render app purchase-manger', () => {
+    cy.get('[data-cy="open-panel"]').should('be.visible');
+    cy.get('[data-cy="open-panel"]').click();
+    cy.get('[data-cy="app-option"]').contains('einkauf-verwalten').click();
+    cy.get('lib-purchase-manager').should('be.visible');
+  });
+
+  it('should match the previous screenshot', () => {
+    cy.visit('/');
+  });
+});
