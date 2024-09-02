5fe26d272f5987a34f624eae769f7f782d1a7989
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-09-02 20:34:22 +0200

    fixe jest test

--- libs/ui/src/lib/components/auto-complete/auto-complete.component.spec.ts
+++ libs/ui/src/lib/components/auto-complete/auto-complete.component.spec.ts
@@ -1,13 +1,21 @@
 import { AutoCompleteComponent } from './auto-complete.component';
 import { MockModule } from 'ng-mocks';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { byTestId, createComponentFactory, Spectator } from '@ngneat/spectator';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppOption } from '@family-central-frontend/ui';
+import { By } from '@angular/platform-browser';
 
 describe('AutoCompleteComponent', () => {
   let spectator: Spectator<AutoCompleteComponent>;
+
+  const appOptions: Array<AppOption> = [
+    { name: 'Option 1', url: 'url-app-1' },
+    { name: 'Option 2', url: 'url-app-2' },
+    { name: 'Option 3', url: 'url-app-3' },
+  ];
   const createComponent = createComponentFactory({
     component: AutoCompleteComponent,
     imports: [
@@ -20,62 +28,85 @@ describe('AutoCompleteComponent', () => {
   });
 
   beforeEach(() => {
-    spectator = createComponent();
+    spectator = createComponent({
+      props: { placeHolder: 'Hello World', appOptions: appOptions },
+    });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  xit('should display options in the autocomplete panel', () => {
-    //  spectator.component.; // Set the input
-    spectator.component.ngOnInit(); // Trigger ngOnInit to initialize filteredOptions
+  it('should display placeholder in the autocomplete panel', () => {
+    const inputElement = spectator.query('input');
 
-    // Simulate input value change
-    // spectator.component.optionControl.setValue(appOptions()[1]);
-    spectator.detectChanges();
-
-    // Open the autocomplete panel
-    const input = spectator.query(byTestId('autocomplete-input'));
-    spectator.click(input!);
-    spectator.detectChanges();
-
-    const options = spectator.queryAll('.mat-option');
-    expect(options.length).toBe(1);
-    expect(options[0].textContent).toContain('Option 1');
-  });
-
-  xit('should emit selected option URL', () => {
-    // spectator.component.appOptions = appOptions;
-    spectator.component.ngOnInit();
-
-    spyOn(spectator.component.selectedAppOptionUrl, 'emit');
-
-    // Simulate selecting an option
-    const option = spectator.queryAll('.mat-option')[0];
-    spectator.click(option);
-    spectator.detectChanges();
-
-    expect(spectator.component.selectedAppOptionUrl.emit).toHaveBeenCalledWith(
-      '/option1'
+    expect(inputElement).toHaveAttribute(
+      'ng-reflect-placeholder',
+      'Hello World'
     );
   });
 
-  xit('should clear input value and focus on the input', () => {
-    // Initialize input value
-    //  spectator.component.optionControl.setValue(appOptions()[0]);
+  it('should display options in the autocomplete panel', () => {
+    spectator.component.ngOnInit(); // Trigger ngOnInit to initialize filteredOptions
+    // Simulate input value change
+    // spectator.component.optionControl.setValue(appOptions()[1]);
+    spectator.fixture.whenStable();
+
+    // Open the autocomplete panel
+    const matOption = spectator.queryAll('mat-option');
+    spectator.click(matOption[0]);
     spectator.detectChanges();
 
-    // Spy on the focus method of input element
-    spyOn(spectator.component.input.nativeElement, 'focus');
+    const options = spectator.queryAll('mat-option');
+    expect(options.length).toBe(3);
+    expect(options[0].textContent).toContain('Option 1');
+  });
 
-    // Simulate clear input button click
-    const clearButton = spectator.query(byTestId('clear-input'));
+  it('should emit selected option URL', () => {
+    spectator.component.ngOnInit();
+
+    const emitSpy = jest.spyOn(
+      spectator.component.selectedAppOptionUrl,
+      'emit'
+    );
+
+    const option = spectator.queryAll('mat-option')[0];
+    spectator.click(option);
+    spectator.detectChanges();
+
+    expect(emitSpy).toHaveBeenCalledWith('url-app-1');
+  });
+
+  xit('should clear input value and focus on the input', () => {
+    spectator.component.ngOnInit();
+    spectator.detectChanges();
+
+    // Finde das input-Element durch die Template-Referenzvariable
+    const inputElement = spectator.query('input');
+    expect(inputElement).toBeTruthy();
+
+    // Spy on the focus method of the input element using jest.spyOn
+    const focusSpy = jest.spyOn(
+      spectator.debugElement.query(By.css('input')).nativeElement,
+      'focus'
+    );
+
+    // Simuliere den Klick auf den Clear-Button
+    const clearButton = spectator.query('.btn-clear-input');
+
+    console.log(clearButton);
+
     spectator.click(clearButton!);
     spectator.detectChanges();
 
+    // Überprüfe, ob der Input-Wert gelöscht wurde
     expect(spectator.component.optionControl.value).toBeNull();
-    expect(spectator.component.input.nativeElement.focus).toHaveBeenCalled();
+
+    // Überprüfe, ob die focus-Methode aufgerufen wurde
+    expect(focusSpy).toHaveBeenCalled();
   });
 
   xit('should toggle panel open and closed', () => {
--- libs/ui/src/test-setup.ts
+++ libs/ui/src/test-setup.ts
@@ -1,3 +1,6 @@
+import '@testing-library/jest-dom';
+import 'jest-preset-angular/setup-jest';
+
 // @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
   testEnvironmentOptions: {
@@ -5,4 +8,3 @@ globalThis.ngJest = {
     errorOnUnknownProperties: true,
   },
 };
-import 'jest-preset-angular/setup-jest';
