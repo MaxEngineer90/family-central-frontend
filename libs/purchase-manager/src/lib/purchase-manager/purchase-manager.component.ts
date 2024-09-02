7a785e506607d59911b294b7521a2f4ee9a244dc
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-09-02 18:44:03 +0200

    move variable.scss

--- libs/purchase-manager/src/lib/purchase-manager/purchase-manager.component.ts
+++ libs/purchase-manager/src/lib/purchase-manager/purchase-manager.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppOption, AutoCompleteComponent } from '@family-central-frontend/ui';
 
 @Component({
   selector: 'fcf-purchase-manager',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AutoCompleteComponent],
   templateUrl: './purchase-manager.component.html',
   styleUrl: './purchase-manager.component.scss',
 })
-export class PurchaseManagerComponent {}
+export class PurchaseManagerComponent {
+  protected readonly appOptions: Array<AppOption> = [
+    { name: '', url: '' },
+    { name: '', url: '' },
+  ];
+
+  navigateToSelectedApp($event: string) {}
+}
